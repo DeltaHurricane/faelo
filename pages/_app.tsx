f5882f323373a9538645100e41f5cc918f2fa661
Lucas Ferreira <lucasxcastor@gmail.com>
2023-03-16 19:28:44 -0300

    saving

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import "@/styles/styles.css";
+import type { AppProps } from "next/app";
+import { ChakraBaseProvider, extendBaseTheme } from "@chakra-ui/react";
+import chakraTheme from "@chakra-ui/theme";
+import Head from "next/head";
+
+const { Button } = chakraTheme.components;
+
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraBaseProvider theme={theme}>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="Faelo" content="Site da Enactus Ufes Vitoria" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo.png" />
+      </Head>
+      <Component {...pageProps} />
+    </ChakraBaseProvider>
+  );
 }
